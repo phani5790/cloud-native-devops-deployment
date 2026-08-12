a2bf1055e00c928c81828ff4538daea6f30c3e15
phani5790 <phanikumarmaddala14@gmail.com>
2026-08-12 11:18:14 +0000

    feat: add cloud native application

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "application": "Cloud Native DevOps Application",
+        "status": "running",
+        "environment": os.getenv("ENVIRONMENT", "development")
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
